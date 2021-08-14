4d5c85dfacca6c7b6ab3e0cf3e3e7b0983cce02c
Tomer <tomerevach@gmail.com>
2021-08-14 19:48:38 +0300

    fix delete bug and updated keyword for pics

--- js/gallery-service.js
+++ js/gallery-service.js
@@ -16,30 +16,30 @@ let gKeywords = {
     Putin: 16
 }
 const gImgs = [
-    { id: 1, url: 'img/1.jpg', keywords: ['Happy', 'Trump'] },
-    { id: 2, url: 'img/2.jpg', keywords: ['Pets', 'Love'] },
-    { id: 3, url: 'img/3.jpg', keywords: ['Children', 'Pets', 'Sleep'] },
+    { id: 1, url: 'img/1.jpg', keywords: ['Happy'] },
+    { id: 2, url: 'img/2.jpg', keywords: ['Trump'] },
+    { id: 3, url: 'img/3.jpg', keywords: ['Pets', 'Love'] },
     { id: 4, url: 'img/4.jpg', keywords: ['Sleep', 'Pets'] },
     { id: 5, url: 'img/5.jpg', keywords: ['Children', 'Winner'] },
-    { id: 6, url: 'img/6.jpg', keywords: ['Smart'] },
-    { id: 7, url: 'img/7.jpg', keywords: ['Children', 'Suprising'] },
-    { id: 8, url: 'img/8.jpg', keywords: ['Happy'] },
-    { id: 9, url: 'img/9.jpg', keywords: ['Happy', 'Children'] },
-    { id: 10, url: 'img/10.jpg', keywords: ['Happy'] },
-    { id: 11, url: 'img/11.jpg', keywords: ['Love', 'Winner'] },
-    { id: 12, url: 'img/12.jpg', keywords: ['Pointing'] },
-    { id: 13, url: 'img/13.jpg', keywords: ['Cool', 'Happy', 'Winner'] },
-    { id: 14, url: 'img/14.jpg', keywords: ['Cool'] },
-    { id: 15, url: 'img/15.jpg', keywords: ['Zero'] },
-    { id: 16, url: 'img/16.jpg', keywords: ['Happy'] },
-    { id: 17, url: 'img/17.jpg', keywords: ['Putin', 'Pointing'] },
-    { id: 18, url: 'img/18.jpg', keywords: ['Happy'] },
-    { id: 19, url: 'img/19.jpg', keywords: ['Happy'] },
-    { id: 20, url: 'img/20.jpg', keywords: ['Happy'] },
-    { id: 21, url: 'img/21.jpg', keywords: ['Happy'] },
+    { id: 6, url: 'img/6.jpg', keywords: ['Pets','Sleep'] },
+    { id: 7, url: 'img/7.jpg', keywords: ['Happy'] },
+    { id: 8, url: 'img/8.jpg', keywords: ['Happy','Children'] },
+    { id: 9, url: 'img/9.jpg', keywords: ['Smart', 'Pointing'] },
+    { id: 10, url: 'img/10.jpg', keywords: ['Pointing'] },
+    { id: 11, url: 'img/11.jpg', keywords: ['Smart', 'Winner'] },
+    { id: 12, url: 'img/12.jpg', keywords: ['Cool'] },
+    { id: 13, url: 'img/13.jpg', keywords: ['Children', 'Happy', 'Winner'] },
+    { id: 14, url: 'img/14.jpg', keywords: ['Cool','Winner'] },
+    { id: 15, url: 'img/15.jpg', keywords: ['Suprising'] },
+    { id: 16, url: 'img/16.jpg', keywords: ['Pets'] },
+    { id: 17, url: 'img/17.jpg', keywords: ['Happy'] },
+    { id: 18, url: 'img/18.jpg', keywords: ['Happy','Love','Winner'] },
+    { id: 19, url: 'img/19.jpg', keywords: ['Cool'] },
+    { id: 20, url: 'img/20.jpg', keywords: ['Cool'] },
+    { id: 21, url: 'img/21.jpg', keywords: ['Zero'] },
     { id: 22, url: 'img/22.jpg', keywords: ['Happy'] },
     { id: 23, url: 'img/23.jpg', keywords: ['Happy'] },
-    { id: 24, url: 'img/24.jpg', keywords: ['Happy'] },
+    { id: 24, url: 'img/24.jpg', keywords: ['Putin'] },
     { id: 25, url: 'img/25.jpg', keywords: ['Happy'] },
 ];
 
@@ -75,9 +75,9 @@ const getImageById = (id) => {
 }
 
 const deleteMeme = (memeId) => {
-    let savedMemes = loadFromStorage(MEMES_DB)
-    const ind = savedMemes.indexOf(meme => meme.id === memeId)
-    savedMemes.splice(ind, 1)
-    saveToStorage(MEMES_DB, savedMemes)
+    gMemes = loadFromStorage(MEMES_DB)
+    const ind = gMemes.findIndex(meme =>  meme.id === memeId)
+    gMemes.splice(ind, 1)
+    saveToStorage(MEMES_DB, gMemes)
 
 }
